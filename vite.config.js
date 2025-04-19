bb9967aa8726e179d6e46fd185b6b29fee556f0c
zainabkhan9118 <zainabkhan3473@gmail.com>
2025-04-19 12:34:53 +0500

    changes made to the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://s4backend-c7f27664aa4d.herokuapp.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
